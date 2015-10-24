8e69a91a461fd7f51ecb728574292826cd14dfb3
Thomas Stringer <sqlsalt@outlook.com>
2015-10-24 07:44:15 -0400

    Added support for hashtags

--- index.js
+++ index.js
@@ -1,5 +1,13 @@
 var twitterMaxCharCount = 140;
 
+function formatHashtag(hashtag) {
+    if (hashtag.substring(0, 1) !== '#') {
+        hashtag = '#' + hashtag;
+    }
+    hashtag.replace(' ', '');
+    return hashtag;
+}
+
 function formatTweet(tweet) {
     var cutOff;
     if (tweet.cutOff === undefined) {
@@ -28,12 +36,19 @@ function formatTweet(tweet) {
     
     formattedTweet += tweet.text;
     
-    if (formattedTweet.length > twitterMaxCharCount) {
-        if (formattedTweet[twitterMaxCharCount - cutOff.length - 1] === ' ') {
-            formattedTweet = formattedTweet.substring(0, twitterMaxCharCount - cutOff.length - 1) + cutOff;
+    var hashtagText = '';
+    if (tweet.hashtags !== undefined) {
+        for (var i = 0; i < tweet.hashtags.length; i++) {
+            hashtagText += ' ' + formatHashtag(tweet.hashtags[i]);
+        }
+    }
+    
+    if ((formattedTweet.length + hashtagText.length) > twitterMaxCharCount) {
+        if (formattedTweet[twitterMaxCharCount - cutOff.length - hashtagText.length - 1] === ' ') {
+            formattedTweet = formattedTweet.substring(0, twitterMaxCharCount - cutOff.length - hashtagText.length - 1) + cutOff + hashtagText;
         }
         else {
-            formattedTweet = formattedTweet.substring(0, formattedTweet.substring(0, twitterMaxCharCount - cutOff.length - 1).lastIndexOf(' ')) + cutOff;
+            formattedTweet = formattedTweet.substring(0, formattedTweet.substring(0, twitterMaxCharCount - cutOff.length - hashtagText.length - 1).lastIndexOf(' ')) + cutOff + hashtagText;
         }
     }
     
--- readme.md
+++ readme.md
@@ -14,10 +14,20 @@ npm install tweetify
 var tweetify = require('tweetify');
 
 var tweet = {
-    text: 'check out my new blog post!',
+    text: 'this is my really long tweet that i want the ENTIRE world to read no matter what if you know what i mean and i think you know exactly what i mean every single time',
+    // this prefix is optional, but it allows you to specify
+    // how the tweet should be prefixed. i.e. if you want 
+    // '{Blog Post}: ' in front of your tweet, the below 
+    // spec will give you that
     prefix: {
-        container: 'brackets', // or 'curley-brackets'
-        divider: ': '
-    }
+        container: 'curley-brackets', // or 'brackets' for []
+        text: 'Blog Post',
+        divider: ': ' // other example... ' - ', ' :: ', etc.
+    },
+    hashtags: ['javascript', 'node'] // don't need prefixing #
 };
+
+var formattedTweet = tweetify(tweet);
+
+console.log(formattedTweet);
 ```
